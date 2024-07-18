47ad4b10010c0813e1326f372d5a870efa58db54
Andy Trinh <akt7120@utexas.edu>
2024-07-18 17:21:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,15 @@ const tabContents = document.querySelectorAll('[data-tab-content]')
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
-    tabsContents.forEach(tabContent => {
+    tabContents.forEach(tabContent => {
       tabContent.classList.remove('active')
     })
 
     tabs.forEach(tab => {
       tab.classList.remove('active')
     })
-
-    tab.classList.add('active')
     
+    tab.classList.add('active')
     target.classList.add('active')
   })
 })
